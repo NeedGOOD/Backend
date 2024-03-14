a223d61feefbd1a8e85272c2926d3631341575e8
NeedGOOD <vladyakusevich@gmail.com>
2024-03-14 23:16:03 +0200

    working with functions

---
+++ Lab 2/Task 4/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Math function</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <form action="./function/calculate.php" method="post">
+        <table>
+            <tr>
+                <td><label for="x">x</label></td>
+                <td><label for="y">y</label></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="x" id="x"></td>
+                <td><input type="text" name="y" id="y"></td>
+                <td><input type="submit" value="=" name="equal" style="width: 100px;"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
